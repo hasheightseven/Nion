d9ed85a264ca295c2eb517e153414decd9e93b37
#EightSeven <hasheightseven@gmail.com>
2024-12-19 19:45:19 +0530

    Update neonmain.js

--- neonmain.js
+++ neonmain.js
@@ -1,1 +1,12 @@
 export const neonmain = document.createElement('div');
+
+
+neonmain.textContent = 'nion!!';
+
+neonmain.style.color = '#abcdef';
+
+neonmain.addEventListener('click', () => {
+  neonmain.style.fontsize = neonmain.style.fontsize + 26;
+});
+
+neonmonkeymain.appendChild(neonmain);
