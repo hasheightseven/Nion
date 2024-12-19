0f43c48e91c8fc21bfacdaa06fdcaeb5ad8e081d
#EightSeven <hasheightseven@gmail.com>
2024-12-19 19:20:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-export const neonmonkeyheader = document.getElementById('neon-monkey-header');
-export const neonmonkeymain = document.getElementById('neon-monkey-main');
-export const neonmonkeyfooter = document.getElementById('neon-monkey-footer');
+import neonbar import './neonheader.js';
+import neonmain import './neonmain.js';
+import neonfooter import './neonfooter.js';
+
+const neonmonkeyheader = document.getElementById('neon-monkey-header');
+neonmonkeyheader.appendChild(neonbar);
+
+const neonmonkeymain = document.getElementById('neon-monkey-main');
+neonmonkeymain.appendChild(neonmain);
+
+const neonmonkeyfooter = document.getElementById('neon-monkey-footer');
+neonmonkeyfooter.appendChild(neonfooter);
