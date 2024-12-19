b3c7372d3901c644b4d665a6071d8e6ac531b02e
#EightSeven <hasheightseven@gmail.com>
2024-12-19 19:05:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 export const neonmonkeyheader = document.getElementById('neon-monkey-header');
 export const neonmonkeymain = document.getElementById('neon-monkey-main');
 export const neonmonkeyfooter = document.getElementById('neon-monkey-footer');
-
-const ellomast = document.createElement('div');
-
-ellomast.textContent = 'nion!!';
-
-ellomast.style.color = '#abcdef';
-
-ellomast.addEventListener('click', () => {
-  ellomast.style.fontsize = ellomast.style.fontsize + 26;
-});
-
-neonmonkeymain.appendChild(ellomast);
