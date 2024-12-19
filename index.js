daadbc1df55b12c6dc8548cde65f73904e3a9e96
#EightSeven <hasheightseven@gmail.com>
2024-12-19 17:55:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const neonmonkey = document.getElementById('neon-monkey-main');
 
 neonmonkey.style.backgroundColor = '#001928';
+
+const ellomast = document.getElementById('ello-mastaru');
+
+ellomast.textContent += '!!';
