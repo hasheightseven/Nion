e8adfd086141a9449b0f5aec333d68422a35fa28
#EightSeven <hasheightseven@gmail.com>
2024-12-19 19:47:16 +0530

    Update popeye.js

--- popeye.js
+++ popeye.js
@@ -1,13 +1,1 @@
-import neonmonkeymain from './index.js';
-
-const ellomast = document.createElement('div');
-
-ellomast.textContent = 'nion!!';
-
-ellomast.style.color = '#abcdef';
-
-ellomast.addEventListener('click', () => {
-  ellomast.style.fontsize = ellomast.style.fontsize + 26;
-});
-
-neonmonkeymain.appendChild(ellomast);
+// ello popeye
