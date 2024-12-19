a813b54a135673986849a888097b9bebd7a82b08
#EightSeven <hasheightseven@gmail.com>
2024-12-19 19:15:06 +0530

    Create neonheader.js

---
+++ neonheader.js
@@ -0,0 +1,15 @@
+import neonmonkeyheader from './index.js';
+
+const neonbar = document.createElement('nav');
+
+neonmonkeyheader.appendChild(nonbar);
+
+const logodiv = document.createElement('div');
+logodiv.textContent = 'logo';
+logodiv.style.width = '50%';
+const nonlogodiv = document.createElement('div');
+nonlogodiv.textContent = 'no logo';
+nonlogodiv.style.width = '50%';
+
+neonbar.appendChild(logodiv);
+neonbar.appendChild(nonlogodiv);
