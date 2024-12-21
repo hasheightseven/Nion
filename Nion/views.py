319c9ba76453cd0939b0fc066f0feef00f4169c1
#EightSeven <hasheightseven@gmail.com>
2024-12-22 02:26:07 +0530

    Update views.py

--- Nion/views.py
+++ Nion/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>N ION</h1>")
+    return HttpResponse("<title>N ION</title>")
+def account(request):
+    return HttpResponse("<title>N ION- account</title>")
+def username(request):
+    return HttpResponse("<title>N ION - username</title>")
+def settings(request):
+    return HttpResponse("<title>N ION - settings</title>")
