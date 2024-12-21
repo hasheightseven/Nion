af0b5590826dd81c1e8b179665c0293c997c56e0
#EightSeven <hasheightseven@gmail.com>
2024-12-21 22:04:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/account')
+def account():
+    # return render_template('index.html')
+    return 'ello mastaruuuuu! how are you?'
+
+@app.route('/username')
+def username():
+    # return render_template('index.html')
+    return 'ello mastaruuuuu! how are you?'
+
+@app.route('/settings')
+def settings():
+    # return render_template('index.html')
+    return 'ello mastaruuuuu! how are you?'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1269)
