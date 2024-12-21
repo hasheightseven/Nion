51a08fc80274dc51fed802c31fb7dca7c89e9638
#EightSeven <hasheightseven@gmail.com>
2024-12-21 18:38:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 @app.route('/')
 def home():
-    return "Hello, GitHub Pages with Docker!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1269)
