6c1d87e137472a4ceec88a3513632bb1e7d9cf3a
#EightSeven <hasheightseven@gmail.com>
2024-12-21 17:58:52 +0530

    Update build.py

--- build.py
+++ build.py
@@ -5,4 +5,4 @@ import os
 # Example: Create a simple static HTML file
 os.makedirs("build", exist_ok=True)
 with open("build/index.html", "w") as f:
-    f.write("<html><body><h1>Welcome to My Site!</h1></body></html>")
+    f.write("<html><body id='n-ion-body'>NION</h1></body></html>")
