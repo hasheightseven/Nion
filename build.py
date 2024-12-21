8b7a1cefc02d32d4822121c98e47fccb0dfade46
#EightSeven <hasheightseven@gmail.com>
2024-12-21 19:17:31 +0530

    Delete build.py

--- build.py
+++
@@ -1,8 +0,0 @@
-# welcome to the NION
-# build.py
-import os
-
-# Example: Create a simple static HTML file
-os.makedirs("build", exist_ok=True)
-with open("build/index.html", "w") as f:
-    f.write("<html><body id='n-ion-body'>NION</h1></body></html>")
