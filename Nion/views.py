f9519b54c802038abfb429157eb2ad08a09a2c17
#EightSeven <hasheightseven@gmail.com>
2024-12-22 19:48:57 +0530

    Update views.py

--- Nion/views.py
+++ Nion/views.py
@@ -1,42 +1,11 @@
-from django.http import HttpResponse
-
-
-START_HTML = f"\
-        <!Doctype html>\
-        <html lang='en'/>\
-            <head>\
-                <meta charset='utf-8'/>\
-                <meta name='viewport' content='width=device-width, initial-scale=1'/>\
-                <title>N ION</title>\
-                <script>\
-                        const nionnavbar = document.getElementById('nion-navbar');\
-                        nionnavbar.style.position = 'fixed';\
-                        nionnavbar.style.width = '-webkit-fill-available';\
-                        nionnavbar.style.height = '9vh';\
-                        nionnavbar.style.display = 'flex';\
-                        nionnavbar.style.alignItems = 'center';\
-                        nionnavbar.style.justifyContent = 'space-around';\
-                </script>\
-            </head>"
-
-STOP_HTML = f"\
-        </body>\
-        </html>"
-
-homehtml = START_HTML+"<nav id='nion-navbar'>\
-            <div id='nion-div-logo'>\
-                <svg xmlns='https://www.w3.org/2000/scg/'>\
-                    <path d='M26 26 L26 87 L50 50 Z' fill='#001928'></path>\
-                </svg>\
-            </div>\
-        </nav>"+STOP_HTML
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse(homehtml)
+    return render(request, 'home.html')
 def account(request):
-    return HttpResponse(homehtml)
+    return render(request, 'account.html')
 def username(request):
-    return HttpResponse(homehtml)
+    return render(request, 'username.html')
 def settings(request):
-    return HttpResponse(homehtml)
+    return render(request, 'settings.html')
